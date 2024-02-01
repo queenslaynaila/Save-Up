946713e6f5f0750c8821cb56a0f24fea980d580d
queenslay jema <queenslayjema@gmail.com>
2024-02-02 00:42:53 +0300

    Refactor:sample trial data

---
+++ src/components/data.jsx
@@ -0,0 +1,27 @@
+const savingsData = [
+    {
+      id: 1,
+      description: 'Emergency Fund',
+      category: 'Monthly',
+      targetAmount: 5000,
+      contributedAmount: 2000,
+    },
+    
+    {
+      id: 4,
+      description: 'Education Fund',
+      category: 'Annual',
+      targetAmount: 15000,
+      contributedAmount: 3000,
+    },
+    {
+      id: 5,
+      description: 'Home Renovation',
+      category: 'Monthly',
+      targetAmount: 8000,
+      contributedAmount: 2500,
+    },
+  ];
+  
+  export default savingsData;
+  
