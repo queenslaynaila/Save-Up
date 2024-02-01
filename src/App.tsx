0c43f92433379cfbcc03bf90885dfe8da80ba614
queenslay jema <queenslayjema@gmail.com>
2024-02-01 12:31:04 +0300

    chore: Format code with Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
- 
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
