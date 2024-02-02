e919bde0597d3356b5a4b7a2b778938398ebcd24
queenslay jema <queenslayjema@gmail.com>
2024-02-02 10:55:04 +0300

    Style:optimize nav icons for smaller screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { css, jsx } from '@emotion/react';
 import { HiOutlineMenuAlt2 } from 'react-icons/hi';
@@ -9,70 +10,97 @@ import { GoGoal } from 'react-icons/go';
 import { CiStickyNote } from 'react-icons/ci';
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
   const navStyle = css({
     display: 'flex',
-    flexDirection: 'row',
+    flexDirection: 'column',
     justifyContent: 'space-between',
     padding: '1.5rem',
-    borderBottom: '2px solid #ccc',  
+    borderBottom: '2px solid #ccc',
     backgroundColor: 'white',
-    margin: '0',  
-     
+    margin: '0',
+    
+    ul: {
+      listStyle: 'none',
+      display: showLinks ? 'flex' : 'none', // Toggle display based on showLinks state
+      flexDirection: 'column',
+
+      li:{
+        marginBottom:'4px',
+        padding:'4px',
+        marginTop:'4px'
+      }
+    },
+
+
+
+    '@media (min-width: 768px)': {
+      flexDirection: 'row',
+      ul: {
+        listStyle: 'none',
+        display: 'flex',
+        flexDirection: 'row',
+        li:{
+          marginBottom:'0px',
+          padding:'0px',
+        }
+      },
+    },
   });
 
-  const navItemContainer = css({
+  const logoContainer = css({
     display: 'flex',
-    listStyle: 'none',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  });
+
+  const logoName = css({
+    color: '#dc3545',
+    marginLeft: '0.5rem',
+    fontSize: '24px',
+    fontWeight: 'bold',
+  });
+
+  const toggleIcon = css({
+    display: 'block',
+    cursor: 'pointer',
+    fontSize: '24px',
+
+    '@media (min-width: 768px)': {
+      display: 'none',
+    },
   });
 
   const navItem = css({
     color: '#0000008c',
     cursor: 'pointer',
     marginRight: '1rem',
-    textDecoration: 'none',  
-    fontWeight: 'bold', 
-    fontSize: '20px' 
-   
-  });
-
-  const logoContainer = css({
+    textDecoration: 'none',
+    fontWeight: 'bold',
+    fontSize: '20px',
     display: 'flex',
     alignItems: 'center',
-  });
 
-  const logoName = css({
-    color: '#dc3545',
-    marginLeft: '0.5rem',
-    fontSize: '24px',  
-    fontWeight:'bold'
-  });
-
-  const toggleIcon = css({
-    display: 'none',
- 
-  
-});
-
-  const mobileView = css({
-    '@media (max-width: 768px)': {
-      flexDirection: 'column',
-      
+    '@media (min-width: 768px)': {
+      fontSize: '18px',
+      marginRight: '1.5rem',
     },
-    
-
   });
 
   return (
-    <nav css={[navStyle, mobileView]}>
+    <nav css={navStyle}>
       <div css={logoContainer}>
-        <BsFlower1 fontSize="24px" color='#dc3545'/> {/* Adjust icon size */}
-        <span css={logoName}>Save Up</span>
+        <div>
+          <BsFlower1 fontSize="24px" color="#dc3545" />
+          <span css={logoName}>Save Up</span>
+        </div>
+        <HiOutlineMenuAlt2 css={toggleIcon} onClick={() => setShowLinks(!showLinks)} />
       </div>
-      <HiOutlineMenuAlt2 css={toggleIcon} />
-      <ul css={navItemContainer}>
+      <ul>
         <li>
           <Link to="/" css={navItem}>
-            <AiFillHome   />
+            <AiFillHome />
             Home
           </Link>
         </li>
@@ -82,10 +110,9 @@ const Navbar = () => {
             Goals
           </Link>
         </li>
-       
         <li>
           <Link to="/Summary" css={navItem}>
-            <CiStickyNote   />
+            <CiStickyNote />
             Summary
           </Link>
         </li>
