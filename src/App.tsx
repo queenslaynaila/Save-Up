4c2b067553ed97489d40f12c5b6685c8746858a2
queenslay jema <queenslayjema@gmail.com>
2024-02-02 10:58:09 +0300

    Feat:app.js

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Navbar from "./components/Navbar";
 import Welcome from "./pages/Welcome";
 import Savings from "./pages/Savings";
 import Goal from  './pages/Goal'
+import Summary from "./pages/Summary";
+import { SavingsProvider } from "./context/SavingsContext";
 function App() {
   const globalStyles = css`
   * {
@@ -21,13 +23,17 @@ function App() {
 
   return (
     <div>
-       <Global styles={globalStyles} />
+      <SavingsProvider>
+      <Global styles={globalStyles} />
        <Navbar/>
        <Routes>
         <Route path="/" element={<Welcome />} />
           <Route path="/savings" element={<Savings/>} />
           <Route path="/savings/:id" element={<Goal />} />
+          <Route path="/Summary" element={<Summary />} />
        </Routes>
+      </SavingsProvider>
+      
     </div>
   );
 }
