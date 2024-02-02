4018868e75e233fec088ffb56286f33981632c13
queenslay jema <queenslayjema@gmail.com>
2024-02-02 10:56:59 +0300

    Feat:Saving page

--- src/pages/Savings.tsx
+++ src/pages/Savings.tsx
@@ -1,10 +1,10 @@
- 
-
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx, css } from '@emotion/react';
-import SavingCard from "../components/SavingCard"
-import savingsData from '../components/data';
+import SavingCard from '../components/SavingCard';
+import SavingsContext from '../context/SavingsContext';
+import { useContext } from 'react';
+import CreateSavingCard from '../components/CreateSavingCard';
 const containerStyles = css`
   max-width: 100%;
   margin: auto;
@@ -16,17 +16,54 @@ const headerContainerStyles = css`
   justify-content: space-between;
   margin-bottom: 12px;
 `;
- 
+
+const noSavingsContainerStyles = css`
+  
+  padding: 20px;
+  background-color: #fff;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+`;
+
+const  textHea= css`
+  
+  padding: 10px;
+  margin-bottom:10px;
+`;
+
+
 const Savings = () => {
+  const { savings } = useContext(SavingsContext);
+
+  if (savings.length === 0) {
+    return (
+      <div css={noSavingsContainerStyles}>
+        <div css={textHea}> <h2>You have no savings</h2>
+        <p>Start your saving journey here.</p></div>
+       
+         <CreateSavingCard/>
+      </div>
+    );
+  }
+
   return (
     <div css={containerStyles}>
-    <div css={headerContainerStyles}>
-      <h2>Your Saving Goals</h2>
+      <div css={headerContainerStyles}>
+        <h2>Your Saving Goals</h2>
+      </div>
+      {savings.map((saving) => (
+        <div key={saving.id}>
+          <SavingCard
+            description={saving.description}
+            category={saving.category}
+            targetAmount={saving.targetAmount}
+            contributedAmount={saving.contributedAmount}
+            id={saving.id}
+          />
+        </div>
+      ))}
     </div>
-    {savingsData.map((saving, index) => (
-      <SavingCard saving={saving} key={index}/>
-    ))}
-  </div>
-  )
-}
-export default Savings
+  );
+};
+
+export default Savings;
